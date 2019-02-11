20cbe0efe5149502acb5a2125c235176ff3a0f05
Zarrina <zarrina_@hotmail.com>
2019-02-11 16:09:54 -0500

    add python functions

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import setuptools
+
+REQUIRED = [
+"numpy",
+"pandas"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name = "pylambdata_zarrina",
+    version="0.0.1",
+    author="Zarrina",
+    description = "A collection of Data Science helper functions",
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zarrinan/pylambdata",
+    packages=setuptools.find_packages(),
+    pyhton_requires=">=3.7",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
