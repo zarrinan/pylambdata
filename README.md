440198a31b269c5737e7e1ea9d7843df76fd0c28
Zarrina <zarrina_@hotmail.com>
2019-02-12 18:17:35 -0500

    add link to package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pylambdata
-Python package with helpful utility functions for DS!
+Python package with helpful utility functions for DS! You can check it [here](https://test.pypi.org/project/pylambdata-zarrina/)
--- pylambdata_zarrina/__init__.py
+++ pylambdata_zarrina/__init__.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 import . zee
+
 VERSION = 0
 ONES = np.ones(100)
 ONES_DF = pd.DataFrame(ONES)
@@ -14,22 +15,22 @@ ONES_DF = pd.DataFrame(ONES)
 #LabelEncoder Auto encodes any dataframe column of type category or object
 def dummy_encode(df):
     from sklearn.preprocessing import LabelEncoder
-    columnsToEncode = list(df.select_dtypes(include=['category','object']))
+    columnsToEncode = list(df.select_dtypes(include=['category', 'object']))
     le = LabelEncoder()
     for feature in columnsToEncode:
         try:
-        df[feature] = le.fit_transform(df[feature])
-    except:
-        print('Error encoding ' + feature)
+            df[feature] = le.fit_transform(df[feature])
+        except:
+            print('Error encoding ' + feature)
     return df
 
 
 # Encode categorical features in a dataframe
 def encode_cat(df):
     for col in df.columns:
-    if (df[col].dtype == 'object'):
-        df[col] = df[col].astype('category')
-        df[col] = df[col].cat.codes
+        if (df[col].dtype == 'object'):
+            df[col] = df[col].astype('category')
+            df[col] = df[col].cat.codes
     return df
 
 
@@ -39,14 +40,14 @@ def no_nulls(df):
 
 
 #training, test, validation split
-def train_validation_test_split(X, y, train_size = 0.8, val_size = 0.1, test_size = 0.1,
-        random_state = None, shuffle = True):
+def train_validation_test_split(X, y, train_size=0.8, val_size=0.1, test_size=0.1,
+        random_state=None, shuffle=True):
 
     from sklearn.model_selection import train_test_split
     assert train_size + val_size + test_size == 1
 
 X_train_val, X_test, y_train_val, y_test = train_test_split(
-    X, y, test_size = test_size, random_state = random_state, shuffle = shuffle)
+    X, y, test_size=test_size, random_state=random_state, shuffle = shuffle)
 
 X_train, X_val, y_train, y_val = train_test_split(
     X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
